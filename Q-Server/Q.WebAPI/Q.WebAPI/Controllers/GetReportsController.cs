171f8166473e2bbaf57c0fca3950da1f79ad8627
William Alen Guterman <alentorz@gmail.com>
2017-10-04 21:39:04 +0300

    Add basic http server side logic

---
+++ Q-Server/Q.WebAPI/Q.WebAPI/Controllers/GetReportsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Q.WebAPI.Core;
+
+namespace Q.WebAPI.Controllers {
+    [Route("api/[controller]")]
+    public class GetReportsController: Controller {
+        // GET api/values
+        // E.G. http://localhost:2533/api/GetReports?lastReportHash=asdasa
+        [HttpGet]
+        public async Task <ContentResult> Get (string lastReportHash){
+            {
+                if (lastReportHash != StaticData.LastReportHash.ToString())
+                    return Content(JsonConvert.SerializeObject(StaticData.Reports.Where(report => report.DateTime.AddMinutes(10) > DateTime.Now)), "application/json");
+                return Content(string.Empty);
+            }
+        }
+    }
+}
---
+++ Q-Server/Q.WebAPI/Q.WebAPI/Controllers/ReportController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Q.WebAPI.Core;
+using Q.WebAPI.Core.DataStructures;
+
+namespace Q.WebAPI.Controllers {
+    [Route("api/[controller]")]
+    public class ReportController: Controller {
+        // GET api/Report
+        // E.G. http://localhost:2533/api/report?location.latitude=123&location.longitude=456&Queue=7890
+        [HttpGet]
+        public string Get (Report report){
+            report.DateTime = DateTime.Now;
+            StaticData.Reports.Add(report);
+            StaticData.LastReportHash = Guid.NewGuid();
+            return StaticData.LastReportHash.ToString();
+        }
+    }
+}
